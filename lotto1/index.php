1623ba80c89263a85a633a53dc18bff4814d2bb6
diak <diak@10.9.8.4>
2019-02-12 08:07:34 +0100

    alap

---
+++ lotto1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        function sorsolas()
+        {
+            $kihuzottak=0;
+            while($kihuzottak<5)
+            {
+                $sorsoltszam=rand(1,90);
+                if(!isset($tomb[$sorsoltszam]))
+                {
+                    $tomb[$sorsoltszam]=1;
+                    $kihuzottak++;
+                }
+            }
+            return $tomb;
+        }
+        ?>
+    </body>
+</html>
